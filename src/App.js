04544dc220c4b19c8fad4a72a87c233d2f63905d
Dmitro Didenko <dmitrdida@ukr.net>
2018-10-09 16:35:31 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
+import data from './data'
+import bit from './img/bitcoin.png';
+import eth from './img/ethereum.png';
+import lit from './img/litecoin.png';
 import './App.css';
 
 class App extends Component {
+
     render() {
         return (
             <div className="wrapper">
@@ -19,17 +24,67 @@ class App extends Component {
                             </p>
                         </div>
                         <div className='main-section__cards'>
-                            <div className="cards__card-one">
-                                <p className="card__text">Ethereum</p>
-                                <img src="img/ethereum.png" alt="ethereum"/>
+                            <div className="cards__card cards__one">
+                                <div className="card__course">
+                                    <p className="cards__text">Ethereum</p>
+                                    <img className='img course__img' src={eth} alt="ethereum"/>
+                                </div>
+                                <div className="info">
+                                    <p className='info__text info__pr'>Price:</p>
+                                    <span className="info__price">{data.changes.percent.month_6}</span>
+                                    <p className='info__text info__pr'>Change</p>
+                                    <span className="info__price">{data.changes.price.month_6}</span>
+                                    <p className='info__text'>Hour change</p>
+                                    <span className="info__price">{data.open.hour}</span>
+                                    <p className='info__text'>Day change</p>
+                                    <span className="info__price">{data.open.day}</span>
+                                    <p className='info__text'>Week change</p>
+                                    <span className="info__price">{data.open.week}</span>
+                                    <p className='info__text'>Month change</p>
+                                    <span className="info__price">{data.open.month}</span>
+                                </div>
                             </div>
-                            <div className="cards__card-two">
-                                <p className="card__text">Litecoin</p>
-                                <img src="img/litecoin.png" alt="litecoin"/>
+
+                            <div className="cards__card cards__two">
+                                <div className="card__course">
+                                    <p className="cards__text">Litecoin</p>
+                                    <img className='img' src={lit} alt="litecoin"/>
+                                </div>
+                                <div className="info">
+                                    <p className='info__text info__pr'>Price:</p>
+                                    <span className="info__price">{data.changes.percent.month_6}</span>
+                                    <p className='info__text info__pr'>Change</p>
+                                    <span className="info__price">{data.changes.price.month_6}</span>
+                                    <p className='info__text'>Hour change</p>
+                                    <span className="info__price">{data.open.hour}</span>
+                                    <p className='info__text'>Day change</p>
+                                    <span className="info__price">{data.open.day}</span>
+                                    <p className='info__text'>Week change</p>
+                                    <span className="info__price">{data.open.week}</span>
+                                    <p className='info__text'>Month change</p>
+                                    <span className="info__price">{data.open.month}</span>
+                                </div>
                             </div>
-                            <div className="cards__card-three">
-                                <p className="card__text">Bitcoin</p>
-                                <img src="img/bitcoin.png" alt="bitcoin"/>
+
+                            <div className="cards__card cards__three">
+                                <div className="card__course">
+                                    <p className="cards__text">Bitcoin</p>
+                                    <img className='img' src={bit} alt="bitcoin"/>
+                                </div>
+                                <div className="info">
+                                    <p className='info__text'>Price:</p>
+                                    <span className="info__price info__pr">{data.changes.percent.month_6}</span>
+                                    <p className='info__text'>Change</p>
+                                    <span className="info__price info__pr">{data.changes.price.month_6}</span>
+                                    <p className='info__text'>Hour change</p>
+                                    <span className="info__price">{data.open.hour}</span>
+                                    <p className='info__text'>Day change</p>
+                                    <span className="info__price">{data.open.day}</span>
+                                    <p className='info__text'>Week change</p>
+                                    <span className="info__price">{data.open.week}</span>
+                                    <p className='info__text'>Month change</p>
+                                    <span className="info__price">{data.open.month}</span>
+                                </div>
                             </div>
                         </div>
                     </section>
---
+++ src/data.js
@@ -0,0 +1,45 @@
+export default {
+    ask: 6644,
+    bid: 6641.99,
+    last: 6642.88,
+    high: 6686.98,
+    low: 6576.73,
+    open: {
+        hour: 6641.43,
+        day: 6648.01,
+        week: 6585.6,
+        month: 6302.95,
+        month_3: 6717.42,
+        month_6: 6900.51,
+        year: 4648.56
+    },
+    averages: {
+        day: 6638.24,
+        week: 6568.83,
+        month: 6567.8
+    },
+    volume: 52256.05413403,
+    changes: {
+        percent: {
+            hour: 0.02,
+            day: -0.08,
+            week: 0.87,
+            month: 5.39,
+            month_3: -1.11,
+            month_6: -3.73,
+            year: 42.9
+        },
+        price: {
+            hour: 1.44,
+            day: -5.13,
+            week: 57.28,
+            month: 339.93,
+            month_3: -74.54,
+            month_6: -257.63,
+            year: 1994.32
+        }
+    },
+    volume_percent: 79.34,
+    timestamp: 1539071404,
+    display_timestamp: "2018-10-09 07:50:04"
+}
